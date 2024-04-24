589fc7c9198b6e7e332ef9a883c9a34e56223726
Suho Kwon <suho.kwon@gmail.com>
2024-04-24 21:56:57 +0900

    Add more examples.

--- style/expense-manager-module/src/components/ExpenseEntryItem.js
+++ style/expense-manager-module/src/components/ExpenseEntryItem.js
@@ -5,7 +5,7 @@ import styles from './ExpenseEntryItem.module.css'
 class ExpenseEntryItem extends React.Component {
     render() {
         return (
-            <div className={styles.itemStyle} >
+            <div className={styles["itemStyle"]} >
                 <div><b>Item:</b> <em>Mango Juice</em></div>
                 <div><b>Amount:</b> <em>30.00</em></div>
                 <div><b>Spend Date:</b> <em>2020-10-10</em></div>
