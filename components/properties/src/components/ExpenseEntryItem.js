364f44f9ab23fce19cdf322ef328dc1d775e422c
Suho Kwon <suho.kwon@gmail.com>
2024-04-27 14:29:58 +0900

    Add more examples.

--- components/properties/src/components/ExpenseEntryItem.js
+++ components/properties/src/components/ExpenseEntryItem.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './ExpenseEntryItem.css';
 
 class ExpenseEntryItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div>
-                <div><b>Item:</b> <em>Mango Juice</em></div>
-                <div><b>Amount:</b> <em>30.00</em></div>
-                <div><b>Spend Date:</b> <em>2020-10-10</em></div>
-                <div><b>Category:</b> <em>Food</em></div>
+                <div><b>Item:</b> <em>{this.props.name}</em></div>
+                <div><b>Amount:</b> <em>{this.props.amount}</em></div>
+                <div><b>Spend date:</b> <em>{this.props.spendDate.toString()}</em></div>
+                <div><b>Category:</b> <em>{this.props.category}</em></div>
             </div>
-        )
+        );
+
     }
 }
 
--- components/properties/src/index.js
+++ components/properties/src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExpenseEntryItem from './components/ExpenseEntryItem'
 
+const name = "Grape Juice"
+const amount = 30.00
+const spendDate = new Date("2020-10-10")
+const category = "Food"
+
+console.log(spendDate)
+
 ReactDOM.render(
     <React.StrictMode>
-        <ExpenseEntryItem />
+        <ExpenseEntryItem
+            name={name}
+            amount={amount}
+            spendDate={spendDate}
+            category={category} />
     </React.StrictMode>,
     document.getElementById('root')
-);
+)
