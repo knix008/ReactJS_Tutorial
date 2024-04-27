8d6ed958a79de44d1095578d048972b998e08a3f
Suho Kwon <suho.kwon@gmail.com>
2024-04-27 16:22:08 +0900

    Add more examples.

---
+++ proptypes/myapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Sum from './Sum'
+
+function App() {
+  return (
+    <div className="container">
+      <div style={{ padding: "10px" }}>
+        <div>
+          <Sum num1="10" num2="John" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
