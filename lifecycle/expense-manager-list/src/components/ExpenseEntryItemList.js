a3f9ba02fbde9f09591c08f60d4d76a9c6a6967f
Suho Kwon <suho.kwon@gmail.com>
2024-04-29 12:18:49 +0900

    Add more examples.

---
+++ lifecycle/expense-manager-list/src/components/ExpenseEntryItemList.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './ExpenseEntryItemList.css'
+
+class ExpenseEntryItemList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        console.log("ExpenseEntryItemList :: Initialize :: componentDidMount :: Component mounted");
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("ExpenseEntryItemList :: Update :: shouldComponentUpdate invoked :: Before update");
+        return true;
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log("ExpenseEntryItemList :: Initialize / Update :: getDerivedStateFromProps :: Before update");
+        return null;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("ExpenseEntryItemList :: Update :: getSnapshotBeforeUpdate :: Before update");
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("ExpenseEntryItemList :: Update :: componentDidUpdate :: Component updated");
+    }
+
+    componentWillUnmount() {
+        console.log("ExpenseEntryItemList :: Remove :: componentWillUnmount :: Component unmounted");
+    }
+
+    render() {
+        const lists = this.props.items.map((item) =>
+            <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.amount}</td>
+                <td>{new Date(item.spendDate).toDateString()}</td>
+                <td>{item.category}</td>
+            </tr>
+        );
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                        <th>Category</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {lists}
+                </tbody>
+            </table>
+        );
+    }
+}
+export default ExpenseEntryItemList;
