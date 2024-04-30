b9b2e3b43429d44849fe943887f00ab687f97de0
Suho Kwon <suho.kwon@gmail.com>
2024-04-30 11:57:42 +0900

    Add more examples.

---
+++ event_management/expense-manager-msgid/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageWithEvent from './components/MessageWithEvent'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>
+            <MessageWithEvent name="React" />
+            <MessageWithEvent name="React developer" />
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
