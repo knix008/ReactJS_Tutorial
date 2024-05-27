26020868b4f0a81082c26f3bf098261878685e35
Suho Kwon <suho.kwon@gmail.com>
2024-05-27 12:10:13 +0900

    Add more examples.

---
+++ Lists/ECMA6/src/components/ExpenseListUsingForLoop.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+class ExpenseListUsingForLoop extends React.Component {
+   getTotalExpenses() {
+      var items = this.props['expenses'];
+      var total = 0;
+      
+      for(let i = 0; i < items.length; i++) {
+         total += parseInt(items[i]);
+      }
+      return total;
+   }
+
+   render() {
+      var items = this.props['expenses'];
+      var expenses = []
+      
+      for(const [idx, item] of items.entries()) {
+         expenses.push(<tr><td>item {idx + 1}</td><td>{item}</td></tr>)
+      }
+      
+      var total = this.getTotalExpenses();
+      
+      return <table>
+         <thead>
+            <tr>
+               <th>Item</th>
+               <th>Amount</th>
+            </tr>
+         </thead>
+         <tbody>
+            {expenses}
+         </tbody>
+         <tfoot>
+            <tr>
+               <th>Sum</th>
+               <th>{total}</th>
+            </tr>
+         </tfoot>
+      </table>
+   }
+}
+
+export default ExpenseListUsingForLoop
---
+++ Lists/myapp/src/components/ExpenseListUsingForLoop.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+class ExpenseListUsingForLoop extends React.Component {
+   getTotalExpenses() {
+      var items = this.props['expenses'];
+      var total = 0;
+      
+      for(let i = 0; i < items.length; i++) {
+         total += parseInt(items[i]);
+      }
+      return total;
+   }
+
+   render() {
+      var items = this.props['expenses'];
+      var expenses = []
+      for(let i = 0; i < items.length; i++) {
+         expenses.push(<tr><td>item {i + 1}</td><td>{items[i]}</td></tr>)
+      }
+      var total = this.getTotalExpenses();
+      
+      return <table>
+         <thead>
+            <tr>
+               <th>Item</th>
+               <th>Amount</th>
+            </tr>
+         </thead>
+         <tbody>
+            {expenses}
+         </tbody>
+         <tfoot>
+            <tr>
+               <th>Sum</th>
+               <th>{total}</th>
+            </tr>
+         </tfoot>
+      </table>
+   }
+}
+
+export default ExpenseListUsingForLoop
