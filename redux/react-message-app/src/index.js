f4dbe6c837680cc7f7bd5f46176b15731122851a
Suho Kwon <suho.kwon@gmail.com>
2024-05-29 16:43:48 +0900

    Add more examples.

--- redux/react-message-app/src/index.js
+++ redux/react-message-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
