d80c9f1bccf3d3efb2d8231f54f49265e74fea86
Suho Kwon <suho.kwon@gmail.com>
2024-06-02 13:31:30 +0900

    Add Bootstrap examples.

---
+++ bootstrap/myapp-wrapper/src/App.js
@@ -0,0 +1,23 @@
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css'
+import { Button } from 'react-bootstrap';
+
+function App() {
+   return (
+      <div className="container">
+         <Button variant="primary">Primary</Button>{' '}
+         <Button variant="secondary">Secondary</Button>{' '}
+         <Button variant="success">Success</Button>{' '}
+         <Button variant="warning">Warning</Button>{' '}
+         <Button variant="danger">Danger</Button>{' '}
+         <Button variant="info">Info</Button>{' '}
+         <Button variant="light">Light</Button>{' '}
+         <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      </div>
+   );
+}
+
+export default App;
