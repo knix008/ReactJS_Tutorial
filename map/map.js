91d4fd51e1aa782e7b1a8b9c78b45e4ceec79fac
Suho Kwon <suho.kwon@gmail.com>
2024-06-03 16:24:25 +0900

    Add map examples.

---
+++ map/map.js
@@ -0,0 +1,6 @@
+var numbers = [2, 4, 6]
+var transformed = numbers.map((val) => val + val)
+
+for (var item of transformed) {
+  console.log(item)
+}
---
+++ map/myapp/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseListUsingForLoop from './components/ExpenseListUsingForLoop';
+import './App.css';
+
+function App() {
+  var expenses = [100, 200, 300]
+  return (
+    <div>
+      <ExpenseListUsingForLoop expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
